cefee9326fe2c61314e8d1306e042dc4dd3287a0
Letisia-Ornelle <57174957+Letisia-Ornelle@users.noreply.github.com>
2020-10-28 13:58:08 +0100

    ciaomondo

---
+++ src/ciao.java
@@ -0,0 +1,16 @@
+
+public class ciao {
+private static String s = "Hello World!";
+	
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("Ciao Mondo");
+		System.out.println("Bonjour le Monde");
+		
+		System.out.println(s);
+		
+	}
+}
